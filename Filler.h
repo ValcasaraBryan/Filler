9686cf32f38705310123e08faca9f6cd59da73dc
ValcasaraBryan <bryanvalcasara@live.fr>
2018-12-04 17:34:29 +0100

    suppression de mise en place de double list chainee

--- Filler.h
+++ Filler.h
@@ -18,15 +18,31 @@
 #include "libft/includes/ft_printf.h"
 #include <stdio.h>
 
+typedef struct          s_map
+{
+    char                *map;
+    struct s_map        *next;
+}                       t_map;
+
+typedef struct          s_tailles
+{
+    int                 x_map;
+    int                 y_map;
+    struct  s_map       *map;
+}                       t_coor;
+
 typedef struct          s_piece
 {
     char                *piece;
-    int                 x;
-    int                 y;
-    char                me;
-    char                vs;
     struct s_piece      *next;
 }                       t_piece;
+
+typedef struct          s_tailles_piece
+{
+    int                 x_piece;
+    int                 y_piece;
+    struct s_piece      *piece;
+}
 
 
 #endif
